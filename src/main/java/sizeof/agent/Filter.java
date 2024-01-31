14df43663e628ba0e301f6a0856d80f2a9e2c101
Peter Reutemann <fracpete@gmail.com>
2024-02-01 09:39:25 +1300

    replaced GPL with LGPL preamble

--- src/main/java/sizeof/agent/Filter.java
+++ src/main/java/sizeof/agent/Filter.java
@@ -1,21 +1,21 @@
 /*
  *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
+ *   it under the terms of the GNU Lesser General Public License as published by
  *   the Free Software Foundation, either version 3 of the License, or
  *   (at your option) any later version.
  *
  *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
+ *   GNU Lesser General Public License for more details.
  *
- *   You should have received a copy of the GNU General Public License
+ *   You should have received a copy of the GNU Lesser General Public License
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*
  * Filter.java
- * Copyright (C) 2018 University of Waikato, Hamilton, NZ
+ * Copyright (C) 2018-2024 University of Waikato, Hamilton, NZ
  */
 
 package sizeof.agent;
@@ -31,7 +31,7 @@ public interface Filter {
    * Checks whether to skip this superclass (and everything upwards).
    *
    * @param superclass the class to check
-   * @return true if to skip, otherwise we will contained traversing the hierarchy
+   * @return true if to skip, otherwise we will contain traversing the hierarchy
    */
   public boolean skipSuperClass(Class superclass);
 
